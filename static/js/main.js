a1cee6b49ecfe493ea6295f526cc5533d323628a
Scott Guertin <sguertin@gmail.com>
2020-12-29 23:43:16 -0500

    Building out the getReading function, created setInterval fn

--- static/js/main.js
+++ static/js/main.js
@@ -1,20 +1,41 @@
 import Ajax from './ajax.js';
+import { ReadingEntry } from './components/readingEntry.js';
 
+const temperatureElement = document.getElementById('temperature');
+const humidityElement = document.getElementById('humidity');
+const listElement = document.getElementById('sensor-list');
 
 const getReading = () => {
-    Ajax.get('/reading').then((result) => {
-
+    Ajax.get('/reading').then(({response, status}) => {
         let avgTemp = 0, 
             avgHumidity = 0,
-            results = result.content;
+            results = response;
+
+        while (listElement) {
+            listElement.removeNode(listElement.firstChild);
+        }
         for (let i = 0; i < results.length; i++) {
             avgTemp += results[i].temperature;
             avgHumidity += results[i].humidity;
+            listElement.appendChild(
+                new ReadingEntry(
+                    results[i].temperature, 
+                    results[i].humidity, 
+                    results[i].location,
+                    i,
+                )
+            );
         }
+
         avgTemp = avgTemp / results.length;
         avgHumidity = avgHumidity / results.length;
+
+        temperatureElement.textContent = ` ${avgTemp}F`;
+        humidityElement.textContent = ` ${avgHumidity}%`;        
+
     }).catch((reason) => {
         console.log(reason.errorMsg)
     });
 }
 
+setInterval(() => getReading(), 3000);
