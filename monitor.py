3ff5ce86441c71a52a15183343b7ce8c4e782e80
Scott Guertin <sguertin@gmail.com>
2020-12-29 22:24:57 -0500

    Reorganized and made async

--- monitor.py
+++ monitor.py
@@ -1,99 +1,72 @@
 #!/usr/bin/env python3
-# import asyncio
+import asyncio
 
-import sys
-from typing import Union
-import pigpio
-import DHT
-import time
-from dataclasses import dataclass
-from dataclasses_json import dataclass_json, LetterCase
 from datetime import datetime
+from dataclasses import dataclass
+
+from dataclasses_json import dataclass_json, LetterCase
+from pigpio import pi as Pi
+import DHT
+from DHT import sensor as Sensor
+
+from configuration import Configuration
+
 
 @dataclass_json(letter_case=LetterCase.CAMEL)
 @dataclass
 class Reading:
     temperature: float
     humidity: float
-    recorded: datetime = datetime.now()  
-    
-        
+    location: str
+    recorded: datetime = datetime.now()
 
-class SensorMonitor:    
-    pin: int # e.g. Using Pin 11 (GPIO 17) would be set to a value of 17
-    sensor: int # Sensor should be set to DHT.DHT11, DHT.DHTXX or DHT.DHTAUTO
 
-    def __init__(self, pin: int, sensor: int = DHT.DHTAUTO):
-        self.pin = pin
-        self.sensor = sensor
+class SensorMonitor:
+    sensor: Sensor  # Sensor should be set to DHT.DHT11, DHT.DHTXX or DHT.DHTAUTO
+    location: str
 
-    @classmethod
-    def getDHT11Monitor(cls, pin: int):
-        return cls(pin, DHT.DHT11)  
-
-    @classmethod
-    def getDHTXXMonitor(cls, pin: int):
-        return cls(pin, DHT.DHTXX)
-
-    @classmethod
-    def getDHTAUTOMonitor(cls, pin: int):
-        return cls(pin, DHT.DHTAUTO)
-
-    async def get_reading(self) -> Reading:
-        pi = pigpio.pi()
+    # Using Pin 11 (GPIO 17) would be set to a value of 17
+    def __init__(self, pi: Pi, pin: int, location: str, sensor_model_no: int = DHT.DHTAUTO):
         if not pi.connected:
             raise Exception('GPIO on Pi not connected')
-        
-        s = DHT.sensor(pi, self.pin, model = self.sensor)
+        self.location = location
+        self.sensor = Sensor(pi, pin, model=sensor_model_no)
 
+    async def get_reading(self) -> Reading:
         tries = 5   # try 5 times if error
         while tries:
             try:
-                _, _, status, temperature, humidity = s.read()   #read DHT device
+                # timestamp, GPIO, temperature, humidity
+                _, _, status, temperature, humidity = self.sensor.read()  # read DHT device
                 if(status == DHT.DHT_TIMEOUT):  # no response from sensor
-                    return None
+                    break
                 if(status == DHT.DHT_GOOD):
-                    return Reading(temperature, humidity)                    
-                await time.sleep(2)
-                tries -=1
+                    return Reading(temperature, humidity, self.location)
+                await asyncio.sleep(2)
+                tries -= 1
             except KeyboardInterrupt:
                 break
-        return 
+        return
 
 
+class MonitorService:
+    sensors: list[SensorMonitor] = {}
 
+    def __init__(self, pi: Pi, config: Configuration):
+        for sensor in config.sensors:
+            self.sensors.append(
+                SensorMonitor(
+                    pi,
+                    sensor.pin,
+                    sensor.location,
+                    sensor.sensor_model_no,
+                )
+            )
 
+    async def get_data(self) -> list[Reading]:
+        tasks = []
 
+        for sensor in self.sensors:
+            tasks.append(asyncio.create_task(sensor.get_reading()))
 
-
-
-
-
-# def output_data(timestamp, temperature, humidity):
-#     # Sample output Date: 2019-11-17T10:55:08, Temperature: 25°C, Humidity: 72%
-#     date = datetime.fromtimestamp(timestamp).replace(microsecond=0).isoformat()
-#     print(u"Date: {:s}, Temperature: {:g}\u00b0C, Humidity: {:g}%".format(
-#         date, temperature, humidity))
-
-
-# pi = pigpio.pi()
-# if not pi.connected:
-#     exit()
-
-# s = DHT.sensor(pi, pin, model=sensor)
-
-# tries = 5   # try 5 times if error
-# while tries:
-#     try:
-#         timestamp, gpio, status, temperature, humidity = s.read()  # read DHT device
-#         if(status == DHT.DHT_TIMEOUT):  # no response from sensor
-#             print(f'Timed out')
-#             exit()
-#         if(status == DHT.DHT_GOOD):
-#             output_data(timestamp, temperature, humidity)
-#             exit()      # Exit after successful read\
-#         print(f'Waiting.... [Status = {status}] Tries Remaining: {tries}')
-#         time.sleep(2)
-#         tries -= 1
-#     except KeyboardInterrupt:
-#         break
+        return await asyncio.gather(*tasks)
