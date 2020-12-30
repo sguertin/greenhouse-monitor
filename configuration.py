720febba0d7b3ac8b7580299a3151e2c262c2733
Scott Guertin <sguertin@gmail.com>
2020-12-29 23:13:33 -0500

    Updated configuration for emulation and working directory

--- configuration.py
+++ configuration.py
@@ -1,7 +1,4 @@
-
-
 from dataclasses import dataclass
-import json
 from pathlib import Path
 from typing import List
 
@@ -9,7 +6,7 @@ from dataclasses_json import dataclass_json, LetterCase
 
 
 @dataclass_json(letter_case=LetterCase.CAMEL)
-@dataclass
+@dataclass(frozen=True)
 class SensorSettings:
     location: str
     pin: int
@@ -17,11 +14,12 @@ class SensorSettings:
 
 
 @dataclass_json(letter_case=LetterCase.CAMEL)
-@dataclass
+@dataclass(frozen=True)
 class Configuration:
+    hardware_emulation: bool
     sensors: List[SensorSettings]
 
 
-def get_settings(self):
-    with open(Path.cwd().joinpath('configuration.json'), 'r') as f:
+def get_settings(cwd: Path) -> Configuration:
+    with open(cwd.joinpath('configuration.json'), 'r') as f:
         return Configuration.from_json(f.read())
