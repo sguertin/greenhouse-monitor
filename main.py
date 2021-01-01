90d9c78f7cd46d96a395a58d5d138327dfed44e5
Scott Guertin <sguertin@gmail.com>
2021-01-01 13:51:46 -0500

    Updated main to use providers

--- main.py
+++ main.py
@@ -4,24 +4,33 @@ import json
 from pathlib import Path
 
 import pigpio
-from flask import Flask, send_from_directory, Response
+from flask import Flask, send_from_directory, Response, request
 
 import configuration
 from json_encoder import default_json_encoder
-from monitor import MonitorService, MockMonitorService
-
+from monitor import MonitorServiceProvider
+from power import PowerServiceProvider, PowerSwitchStatus
 
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__, static_url_path='')
+app.temperature_status = 'OFF'
+app.heater_status = 'OFF'
+
 cwd = Path.cwd()
 
 pi = pigpio.pi()
 config = configuration.get_settings(cwd)
 
-if config.hardware_emulation:
-    monitor_service = MockMonitorService(pi, config)
-else:
-    monitor_service = MonitorService(pi, config)
+monitor_service = MonitorServiceProvider.get_monitor_service(
+    config.hardware_emulation,
+    pi,
+    config,
+)
+power_service = PowerServiceProvider.get_power_service(
+    config.hardware_emulation,
+    config.heater_url,
+    config.humidifier_url,
+)
 
 
 @app.route('/js/<path:path>', methods=['GET'])
@@ -60,5 +69,46 @@ async def _send_reading():
         content_type='application/json'
     )
 
+
+@app.route('/humidifier', methods=['GET', 'POST'])
+def humidifier_status():
+    if request.method == 'POST':
+        asyncio.get_event_loop().run_until_complete(
+            power_service.toggle_heater()
+        )
+        return Response('OK', status=200)
+    result = asyncio.get_event_loop().run_until_complete(
+        power_service.get_humidifier_status()
+    )
+    return Response(result, status=200, content_type='application/json')
+
+
+async def get_humidifier_status() -> PowerSwitchStatus:
+    return await power_service.get_humidifier_status()
+
+
+async def set_humidifier_status():
+    await power_service.toggle_humidifier()
+
+
+@app.route('/heater', methods=['GET', 'POST'])
+def heater_status():
+    if request.method == 'POST':
+        asyncio.get_event_loop().run_until_complete(
+            power_service.toggle_heater()
+        )
+        return Response('OK', status=200)
+    result = asyncio.get_event_loop().run_until_complete(
+        power_service.get_heater_status()
+    )
+    return Response(result, status=200, content_type='application/json')
+
+
+async def get_heater_status() -> PowerSwitchStatus:
+    return await power_service.get_heater_status()
+
+
+async def set_heater_status():
+    await power_service.toggle_humidifier()
 
 app.run()
--- monitor.py
+++ monitor.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python3
 import asyncio
-
 from datetime import datetime, timedelta
 from dataclasses import dataclass
-
 import random
 
 from dataclasses_json import dataclass_json, LetterCase
 from pigpio import pi as Pi
 import DHT
-from DHT import sensor as Sensor
+from DHT import S, sensor as Sensor
 
-from configuration import Configuration
+from configuration import SensorSettings
 
 
 @dataclass_json(letter_case=LetterCase.CAMEL)
-@dataclass
+@dataclass(frozen=True)
 class Reading:
     temperature: float
     humidity: float
     location: str
+    pin: int
     recorded: datetime = datetime.now()
 
 
 class SensorMonitor:
     sensor: Sensor  # Sensor should be set to DHT.DHT11, DHT.DHTXX or DHT.DHTAUTO
     location: str
+    pin: int
 
     # Using Pin 11 (GPIO 17) would be set to a value of 17
     def __init__(self, pi: Pi, pin: int, location: str, sensor_model_no: int = DHT.DHTAUTO):
@@ -33,9 +33,10 @@ class SensorMonitor:
             raise Exception('GPIO on Pi not connected')
         self.location = location
         self.sensor = Sensor(pi, pin, model=sensor_model_no)
+        self.pin = pin
 
     async def get_reading(self) -> Reading:
-        tries = 10   # try 5 times if error
+        tries = 10   # try 10 times if error
         while tries:
             try:
                 # timestamp, GPIO, temperature, humidity
@@ -43,7 +44,7 @@ class SensorMonitor:
                 if(status == DHT.DHT_TIMEOUT):  # no response from sensor
                     break
                 if(status == DHT.DHT_GOOD):
-                    return Reading(temperature, humidity, self.location)
+                    return Reading(temperature, humidity, self.location, self.pin)
                 await asyncio.sleep(1)
                 tries -= 1
             except KeyboardInterrupt:
@@ -51,14 +52,47 @@ class SensorMonitor:
         return
 
 
-class MonitorService:
-    sensors: list[SensorMonitor] = {}
+class MockSensorMonitor(SensorMonitor):
+    _last_updated: datetime
+    _last_temp: int
+    _last_humid: int
 
-    def __init__(self, pi: Pi, config: Configuration):
-        for sensor in config.sensors:
+    # Using Pin 11 (GPIO 17) would be set to a value of 17
+    def __init__(self, pi: Pi, pin: int, location: str, sensor_model_no: int = DHT.DHTAUTO):
+        self.pin = pin
+        self.location = location
+        self._last_updated = datetime.now()
+        self._last_temp = random.randint(55, 95)
+        self._last_humid = random.randint(30, 90)
+
+    async def get_reading(self) -> Reading:
+        # timestamp, GPIO, temperature, humidity
+        if datetime.now() - timedelta(hours=0, minutes=2) > self._last_updated:
+            temperature = self._last_temp
+            humidity = self._last_humid
+        else:
+            temperature = random.randint(55, 95)
+            humidity = random.randint(30, 90)
+            self._last_updated = datetime.now()
+
+        await asyncio.sleep(random.randint(0, 5))
+
+        self._last_temp = temperature
+        self._last_humid = humidity
+
+        return Reading(temperature, humidity, self.location, self.pin)
+
+
+class MonitorService:
+    pi: Pi
+    sensors: list[SensorMonitor] = []
+
+    def __init__(self, pi: Pi, sensors: list[SensorSettings]):
+        self.pi = pi
+        for sensor in sensors:
             self.sensors.append(
                 SensorMonitor(
-                    pi,
+                    self.pi,
                     sensor.pin,
                     sensor.location,
                     sensor.sensor_model_no,
@@ -69,44 +103,19 @@ class MonitorService:
         tasks = []
 
         for sensor in self.sensors:
-            tasks.append(asyncio.create_task(sensor.get_reading()))
+            tasks.append(
+                asyncio.create_task(
+                    sensor.get_reading()
+                )
+            )
 
         return await asyncio.gather(*tasks)
 
 
-class MockSensorMonitor:
-    sensor: Sensor  # Sensor should be set to DHT.DHT11, DHT.DHTXX or DHT.DHTAUTO
-    location: str
-    _last_reading: datetime
-    _last_temp: int
-    _last_humid: int
-    # Using Pin 11 (GPIO 17) would be set to a value of 17
-
-    def __init__(self, pi: Pi, pin: int, location: str, sensor_model_no: int = DHT.DHTAUTO):
-        # if not pi.connected:
-        #     raise Exception('GPIO on Pi not connected')
-
-        self.location = location
-        self._last_reading = datetime.now()
-        self._last_temp = random.randint(75, 95)
-        self._last_humid = random.randint(30, 90)
-        # self.sensor = Sensor(pi, pin, model=sensor_model_no)
-
-    async def get_reading(self) -> Reading:
-        # timestamp, GPIO, temperature, humidity
-        if datetime.now() - timedelta(hours=0, minutes=2) > self._last_reading:
-            temperature = self._temperature
-            humidity = self._humidity
-        else:
-            temperature = random.randint(55, 95)
-            humidity = random.randint(30, 90)
-        await asyncio.sleep(random.randint(0, 10))
-        return Reading(temperature, humidity, self.location)
-
-
 class MockMonitorService(MonitorService):
-    def __init__(self, pi: Pi, config: Configuration):
-        for sensor in config.sensors:
+
+    def __init__(self, pi: Pi, sensors: list[SensorSettings]):
+        for sensor in sensors:
             self.sensors.append(
                 MockSensorMonitor(
                     pi,
@@ -115,3 +124,13 @@ class MockMonitorService(MonitorService):
                     sensor.sensor_model_no,
                 )
             )
+
+
+class MonitorServiceProvider:
+
+    @staticmethod
+    def get_monitor_service(emulation: bool, pi: Pi, sensors: list[SensorSettings]) -> MonitorService:
+        if emulation:
+            return MockMonitorService(None, sensors)
+        else:
+            return MonitorService(pi, sensors)
