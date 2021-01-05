e64021bb58f151d6a7e16980e754f7d14224895b
Scott Guertin <sguertin@gmail.com>
2021-01-05 00:08:15 -0500

    Update flask to properly servie static files

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import asyncio
 import json
-
 from pathlib import Path
 
 import pigpio
@@ -11,20 +10,26 @@ from json_encoder import default_json_encoder
 from monitor import MonitorServiceProvider
 from power import PowerServiceProvider, PowerSwitchStatus
 
-# set the project root directory as the static folder, you can set others.
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__, static_url_path='',)
+
 app.temperature_status = 'OFF'
 app.heater_status = 'OFF'
 
 cwd = Path.cwd()
-
-pi = pigpio.pi()
 config = configuration.get_settings(cwd)
+
+try:
+    pi = pigpio.pi()
+except:
+    pi = None
+    print('Failed to connect to raspberry pi, forcing hardware emulation')
+    config.hardware_emulation = True
+
 
 monitor_service = MonitorServiceProvider.get_monitor_service(
     config.hardware_emulation,
     pi,
-    config,
+    config.sensors,
 )
 power_service = PowerServiceProvider.get_power_service(
     config.hardware_emulation,
@@ -33,21 +38,23 @@ power_service = PowerServiceProvider.get_power_service(
 )
 
 
-@app.route('/js/<path:path>', methods=['GET'])
-def send_js(path):
-    return send_from_directory(cwd.joinpath('static', 'js'), path, content_type='application/javascript')
-
-
 @app.route('/<path:path>', methods=['GET'])
 def send_html(path):
-    if path == '':
-        path = 'index.html'
-    return send_from_directory(cwd.joinpath('static'), path, content_type='text/html')
+    file_path = cwd.joinpath('static', path)
+    if Path(file_path).is_file():
+        return send_from_directory(cwd.joinpath('static'), path)
+    else:
+        return send_from_directory(cwd.joinpath('static'), 'index.html')
+
+
+@app.route('/js/<path:path>', methods=['GET'])
+def send_js(path):
+    return send_from_directory(cwd.joinpath('static', 'js'), path)
 
 
 @app.route('/css/<path:path>', methods=['GET'])
 def send_css(path):
-    return send_from_directory(cwd.joinpath('static', 'css'), path, content_type='text/css')
+    return send_from_directory(cwd.joinpath('static', 'css'), path)
 
 
 @app.route('/reading', methods=['GET'])
@@ -60,12 +67,14 @@ def send_reading():
 
 
 async def _send_reading():
-    results = []
+    responseBody = {}
+    responseBody['results'] = []
     readings = await monitor_service.get_data()
     for reading in readings:
-        results.append(reading.to_dict())
+        responseBody['results'].append(reading.to_dict())
+
     return Response(
-        json.dumps(results, default=default_json_encoder),
+        json.dumps(responseBody, default=default_json_encoder),
         content_type='application/json'
     )
 
@@ -77,6 +86,7 @@ def humidifier_status():
             power_service.toggle_heater()
         )
         return Response('OK', status=200)
+
     result = asyncio.get_event_loop().run_until_complete(
         power_service.get_humidifier_status()
     )
